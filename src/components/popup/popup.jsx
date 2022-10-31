1f2872e1e940992b0d12a62726b7770c600f43ab
okikiola94 <farominiyiopeyemi@gmail.com>
2022-10-31 13:13:44 +0100

    Align icons and text

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -8,20 +8,20 @@ const PopUp = (props) => {
         <div className="popup-body">
           <ul>
           
-          <li> <a onClick={() => props.goToContact()} className={props.activeRoute === 'contact' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="mailto:youremail@domain.com?subject=sharing link\n&body=I am sharing a link: https://google.com"><img src={`${process.env.PUBLIC_URL}/assets/images/mailpost.png`} alt="mail-img" />Share as Email </a>
+          <li> <a onClick={() => props.goToContact()} className={props.activeRoute === 'contact' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="mailto:youremail@domain.com?subject=sharing link\n&body=I am sharing a link: https://google.com"><img src={`${process.env.PUBLIC_URL}/assets/images/mail-icon.png`} alt="mail-img" />Share as Email </a>
             </li>
 
             <li>
-              <a onClick={() => props.goToHome()} className={props.activeRoute === 'home' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&quot%3B%20class=&quot%3Btwitter-share-button=&quot%3B%20data-show-count=&quot%3Bfalse=&quot=&url="><img src={`${process.env.PUBLIC_URL}/assets/images/twitter.png`} alt="twitter-img" />Share on Twitter  </a>
+              <a onClick={() => props.goToHome()} className={props.activeRoute === 'home' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&quot%3B%20class=&quot%3Btwitter-share-button=&quot%3B%20data-show-count=&quot%3Bfalse=&quot=&url="><img src={`${process.env.PUBLIC_URL}/assets/images/twitter-icon.png`} alt="twitter-img" />Share on Twitter  </a>
             </li>
             <li>
-              <a onClick={() => props.goToAbout()} className={props.activeRoute === 'about' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="https://faq.whatsapp.com/425247423114725/?helpref=uf_share"><img src={`${process.env.PUBLIC_URL}/assets/images/whatsapp.png`} alt="whatsapp-img" />Share on Whatsapp </a>
+              <a onClick={() => props.goToAbout()} className={props.activeRoute === 'about' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="https://faq.whatsapp.com/425247423114725/?helpref=uf_share"><img src={`${process.env.PUBLIC_URL}/assets/images/whatsapp-icon.png`} alt="whatsapp-img" />Share on Whatsapp </a>
             </li>
             <li>
-              <a onClick={() => props.goToPortfolio()} className={props.activeRoute === 'experience' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="http://www.facebook.com/share.php?okikiola-frontend.netlify.app"><img src={`${process.env.PUBLIC_URL}/assets/images/facebook.png`} alt="facebook-img" />Share on Facebook </a>
+              <a onClick={() => props.goToPortfolio()} className={props.activeRoute === 'experience' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="http://www.facebook.com/share.php?okikiola-frontend.netlify.app"><img src={`${process.env.PUBLIC_URL}/assets/images/facebook-icon.png`} alt="facebook-img" />Share on Facebook </a>
             </li>
             <li>
-              <a onClick={() => props.goToContact()} className={props.activeRoute === 'contact' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="www.instagram.com"><img src={`${process.env.PUBLIC_URL}/assets/images/instagram.png`} alt="instagram-img" />Share on Instagram </a></li>
+              <a onClick={() => props.goToContact()} className={props.activeRoute === 'contact' ? 'navigation-menu-active' : 'navigation-menu-inactive'} href="www.instagram.com"><img src={`${process.env.PUBLIC_URL}/assets/images/instagram-icon.png`} alt="instagram-img" />Share on Instagram </a></li>
 
             
           </ul>
